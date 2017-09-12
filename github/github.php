d791c87536990bfa2b0a6810f3b376cd88a40424
remixer-dec <remixer.dec@gmail.com>
2017-09-12 03:21:01 +0300

    ...

--- github/github.php
+++ github/github.php
@@ -6,19 +6,19 @@ function uploadToGithub($user,$repo,$path,$content,$token){
 	$ctx = stream_context_create($opts);
 	$obj = array(
 		"path" => $path,  
-        "message" => "change ".time(), 
-        "content" => base64_encode($content),
-        "sha" => json_decode(file_get_contents($url,false,$ctx))->sha,
-        "branch" => "master"
-        );
+		"message" => "change ".time(), 
+		"content" => base64_encode($content),
+		"sha" => json_decode(file_get_contents($url,false,$ctx))->sha,
+		"branch" => "master"
+		);
     $json = json_encode($obj);
 
 	$ch = curl_init();
 	var_dump($json);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: token $token"));
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT"); 
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT"); 
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-    curl_setopt($ch, CURLOPT_USERAGENT, "xD");
+	curl_setopt($ch, CURLOPT_USERAGENT, "xD");
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
