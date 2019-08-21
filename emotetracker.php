913485290e0487f28ce7847d7333f5fc9dce1f3d
remixer-dec <remixer.dec@gmail.com>
2019-08-22 02:27:33 +0300

    v3 to v5 API migration [GCP]

--- emotetracker.php
+++ emotetracker.php
@@ -1,18 +1,46 @@
-<?php 
+<?php
 $CLIENTID =''; //twitch client ID
+class DB{
+    public $BUCKET = ''; //GCP GCS Bucket
+    public function readFile($fn,$GCP=true){
+        return $GCP ? file_get_contents("gs://$this->BUCKET/$fn") : file_get_contents("./$fn");
+    }
+    public function writeFile($fn,$data,$GCP=true){
+        return $GCP ? file_put_contents("gs://$this->BUCKET/$fn",$data) : file_put_contents("./$fn",$data);
+    }
+}
+$DB = new DB(); //DB wrapper
 
+/**
+ * Checks changes in Twitch and BTTV emotes
+ *
+ * @return     array (string with change descriptions, string with change descriptions in russian, array of emote image urls)
+ */
 function trackEmotes(){
-  global $removed; global $added; global $changed; global $pics; global $CLIENTID;
-  $ttv=file_get_contents('https://api.twitch.tv/kraken/chat/global/emoticons?client_id='.$CLIENTID);
+  global $removed; global $added; global $changed; global $pics; global $CLIENTID; global $DB;
+  $opts = [
+    "http" => [
+        "method" => "GET",
+        "header" => "Accept: application/vnd.twitchtv.v5+json"
+    ]
+  ];
+  $context = stream_context_create($opts);
+  $ttv=file_get_contents("https://api.twitch.tv/kraken/chat/emoticon_images?emotesets=0&client_id=$CLIENTID",false,$context);
   $bttv=file_get_contents('https://api.betterttv.net/2/emotes');
-  $ttv=json_decode($ttv)->emoticons;
+  $ttv=json_decode($ttv)->emoticon_sets->{"0"};
   $bttv=json_decode($bttv)->emotes;
   $thash=md5(serialize($ttv));
   $btthash=md5(serialize($bttv));
-  $ttold=json_decode(file_get_contents('twitch/ttv.json'));
-  $bttold=json_decode(file_get_contents('twitch/bttv.json'));
-  $hash=json_decode(file_get_contents('twitch/hash.json'));
+  $hash=json_decode($DB->readFile('twitch/hash.json'));
+
   $pics = $removed = $added = $changed = [];
+
+function generateEmoteURL($id){
+    return "https://static-cdn.jtvnw.net/emoticons/v1/$id/1.0";
+}
+function generateBTTVEmoteURL($id){
+    return "https://cdn.betterttv.net/emote/$id/1x";
+}
 
   /**
    * compare 2 emote lists from twitch
@@ -23,34 +51,38 @@ function trackEmotes(){
    * @return     0
    */
   function checkEmotes($a1, $a2) {
+    if(!$a1||!$a2){syslog(LOG_CRIT,"Twitch emote array is empty!") and die("something is wrong");}
     global $removed; global $added; global $changed; global $pics;
-    foreach ($a1 as $k => $v) { 
+    foreach ($a1 as $k => $v) {
       $fn=false; //emote is not in the list
       foreach($a2 as $k2 => $v2){
-        if($v->regex==$v2->regex){//if emote is in the list
-     	    if($v->url != $v2->url){//if url was changed
-            array_push($changed, $v->regex);
-            array_push($pics, $v2->url);
-            array_push($pics, $v->url);
+        if($v->code==$v2->code){//if emote is in the list
+     	    if($v->id != $v2->id){//if id was changed
+            array_push($changed, $v->code);
+            array_push($pics, generateEmoteURL($v2->id));
+            array_push($pics, generateEmoteURL($v->id));
      	    }
      	    $fn = true;//emote is in the list
         }
       }
       if(!$fn){//if an emote was not in the list
-        array_push($added,$v->regex);
-        array_push($pics, $v->url);
+        array_push($added,$v->code);
+        array_push($pics, generateEmoteURL($v->id));
       }
     }
     foreach ($a2 as $k => $v) { //similar for removed emotes
-      if($v->regex==$v2->regex){
-         $fn = true;
+      $fn=false;
+      foreach($a1 as $k2 => $v2){
+        if($v->code==$v2->code){
+     	    $fn = true;
+        }
+      }
+      if(!$fn){
+        array_push($removed,$v->code);
+        array_push($pics, generateEmoteURL($v->id));
       }
     }
-    if(!$fn){
-      array_push($removed,$v->regex);
-      array_push($pics, $v->url);
-    }
-    return 0;   
+    return 0;
   }
 
   /**
@@ -62,24 +94,25 @@ function trackEmotes(){
    * @return     0
    */
   function checkBTTVEmotes($a1, $a2) { //same for bttv
+    if(!$a1||!$a2){syslog(LOG_CRIT,"BTTV emote array is empty!") and die("something is wrong");}
     global $removed; global $added; global $changed; global $pics;
-    foreach ($a1 as $k => $v) { 
+    foreach ($a1 as $k => $v) {
       $fn=false;
       foreach($a2 as $k2 => $v2){
         if($v->code==$v2->code){
      	    if($v->id != $v2->id){
             array_push($changed, $v->code."(BTTV)");
-            array_push($pics, 'https://cdn.betterttv.net/emote/'.$v->id.'/1x');
+            array_push($pics, generateBTTVEmoteURL($v->id));
      	    }
      	    $fn = true;
         }
       }
       if(!$fn){
         array_push($added,$v->code."(BTTV)");
-        array_push($pics, 'https://cdn.betterttv.net/emote/'.$v->id.'/1x');
+        array_push($pics, generateBTTVEmoteURL($v->id));
       }
     }
-    foreach ($a2 as $k => $v) { 
+    foreach ($a2 as $k => $v) {
       $fn=false;
       foreach($a1 as $k2 => $v2){
         if($v->code==$v2->code){
@@ -88,16 +121,16 @@ function trackEmotes(){
       }
       if(!$fn){
         array_push($removed,$v->code."(BTTV)");
-        array_push($pics, 'https://cdn.betterttv.net/emote/'.$v->id.'/1x');
+        array_push($pics, generateBTTVEmoteURL($v->id));
       }
     }
-    return 0;   
+    return 0;
   }
 
   /**
    * generates localized text
    *
-   * @param      string  $a      list with new emotes 
+   * @param      string  $a      list with new emotes
    * @param      string  $r      list with removed emotes
    * @param      string  $c      list with changed emotes
    * @param      int     $l      language id
@@ -120,32 +153,34 @@ function trackEmotes(){
   }
 
   if($hash[0]!=$thash){//if something changed in twitch emote list
+    $ttold=json_decode($DB->readFile('twitch/ttv.json'));
   	checkEmotes($ttv,$ttold);
-  	file_put_contents("twitch/ttv.json", json_encode($ttv));
-  	file_put_contents("twitch/hash.json", json_encode(Array($thash,$btthash)));
-  }
-    
-  if($hash[1]!=$btthash){//if something changed in bttv emote list
-  	checkBTTVEmotes($bttv,$bttold);
-  	file_put_contents("twitch/bttv.json", json_encode($bttv)); 
-  	file_put_contents("twitch/hash.json", json_encode(Array($thash,$btthash)));
   }
 
-  $removed = implode(", ",$removed);
-  $added = implode(", ",$added);
-  $changed = implode(", ",$changed);
-  $txt = localize($added,$removed,$changed,0);
-  $rutxt = localize($added,$removed,$changed,1);
+  if($hash[1]!=$btthash){//if something changed in bttv emote list
+    $bttold=json_decode($DB->readFile('twitch/bttv.json'));
+  	checkBTTVEmotes($bttv,$bttold);
+  }
 
   if(count($pics)!=0 && count($pics)<20){ //not an error
-    require_once('vk/vk.php');
-    require_once('twitter/twitter.php');
-    uploadToVK($pics,$rutxt);
-    uploadToTwitter($pics,$txt);
-    //uploadToGithub(); not implemented 
-    
-  } else return false;
-}
+    $removed = implode(", ",$removed);
+    $added = implode(", ",$added);
+    $changed = implode(", ",$changed);
+    $txt = localize($added,$removed,$changed,0);
+    $rutxt = localize($added,$removed,$changed,1);
+    syslog(LOG_DEBUG,$txt.json_encode($pics));
+    $DB->writeFile("twitch/ttv.json", json_encode($ttv));
+    $DB->writeFile("twitch/bttv.json", json_encode($bttv));
+    $DB->writeFile("twitch/hash.json", json_encode(array($thash,$btthash)));
+    return array($txt,$rutxt,$pics);
 
-trackEmotes();
+    } else{
+        if(count($pics)>20){
+            print(var_export($pics,true));
+            syslog(LOG_CRIT,"TOO MANY EMOTES!");
+        }
+        return false;
+    }
+}
+//$result = trackEmotes();
 ?>
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 **Twitch Emote Changes Tracker**
 
-Runs on server every interval of time. Tracks new emotes, removed emotes and changed emotes from Twitch.tv and BTTV. After it finds something, it posts emote info with pictures to VK and Twitter*   
+Runs on server every interval of time. Tracks new emotes, removed emotes and changed emotes from Twitch.tv (global) and BTTV. After it finds something, it posts change information with pictures to VK and Twitter.   
   
-*=(starting from today)  
+UPD 22.08.19: updated tracker to Twitch v5 API, removed posting scripts (vk/twitter), they're outdated due to other API changes and I'm currently using other ones.  
   
 Official Project's [VK](https://vk.com/club132117932) and [Twitter](https://twitter.com/emotetracker)
